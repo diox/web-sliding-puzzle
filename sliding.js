0450890cc3f6f127fb12536aa340ba26dc6c7d89
Mathieu Pillard <m@virgule.net>
2013-02-16 21:30:04 +0100

    Add caching manifest

--- sliding.js
+++ sliding.js
@@ -82,6 +82,19 @@ function init() {
     } else {
         showGui();
     }
+
+    window.applicationCache.addEventListener('updateready', function(e) {
+        if (window.applicationCache.status == window.applicationCache.UPDATEREADY) {
+            // Browser downloaded a new app cache.
+            // Swap it in and reload the page to get the new hotness.
+            window.applicationCache.swapCache();
+            if (confirm('A new version is available, load it immediately ? (Game progress isn\'t lost)')) {
+                window.location.reload();
+            }
+        } else {
+            // Manifest didn't change. Nothing new to serve.
+        }
+    }, false);
 }
 
 function showGui() {
